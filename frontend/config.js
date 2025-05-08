c6d6682efd031fc27913817edd1e72436419d4e9
SRUTHIACHALLA <153352604+SRUTHIACHALLA@users.noreply.github.com>
2025-05-08 10:49:30 +0530

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const API_BASE_URL = isProduction
+  ? 'https://multivendor-backend-ze40.onrender.com' // 
+  : 'http://localhost:5000'; //
